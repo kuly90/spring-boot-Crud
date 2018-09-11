41e7787a8c9b4689c00651078d808bb631b71f28
Ton That Cu Ly <tonly01011990@gmail.com>
2018-09-11 16:06:42 +0700

    fix DB

--- Database.sql
+++ Database.sql
@@ -7,10 +7,10 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL auto_increment,
-  `username` varchar(160) NOT NULL,
-  `password` varchar(160) NOT NULL,
+  `USER_ID` varchar(20) NOT NULL,
+  `NAME` varchar(160) NOT NULL,
+  `YEAR_OLD` int(11) NOT NULL,
   
-  UNIQUE KEY `USER_ID_UNIQUE` (`id`)
+  UNIQUE KEY `USER_ID_UNIQUE` (`USER_ID`)
   
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
